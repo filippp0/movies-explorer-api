d33f13965d6b9e397cec074ba1be382fe0afe547
Filipp Kolesnikov <filippp0@yandex.ru>
2023-09-08 19:35:25 +0300

    пробный коммит

---
+++ app.js
@@ -0,0 +1,40 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const helmet = require('helmet');
+const bodyParser = require('body-parser');
+const { errors } = require('celebrate');
+const cors = require('cors');
+const { limiter } = require('./utils/constants');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
+const errorHandler = require('./middlewares/error-handler');
+
+const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/bitfilmsdb' } = process.env;
+
+const app = express();
+
+app.use(cors());
+
+app.use(helmet());
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect(DB_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+app.use(requestLogger);
+
+app.use(limiter);
+
+app.use('/', require('./routes/index'));
+
+app.use(errorLogger);
+
+app.use(errors());
+
+app.use(errorHandler);
+
+app.listen(PORT);
---
+++ models/user.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const UnautorizedError = require('../errors/UnautorizedError');
+const { emailRegex } = require('../utils/constants');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    minlength: [2, 'Минимальная длина поля - 2'],
+    maxlength: [30, 'Максимальная длина поля - 30'],
+    required: [true, 'Поле должно быть заполнено'],
+  },
+  email: {
+    type: String,
+    required: [true, 'Поле должно быть заполнено'],
+    unique: true,
+    validate: {
+      validator(email) {
+        return emailRegex.test(email);
+      },
+      message: 'Введите верный email',
+    },
+  },
+  password: {
+    type: String,
+    required: [true, 'Поле должно быть заполнено'],
+    select: false,
+  },
+}, { versionKey: false });
+
+userSchema.statics.findUserByCredentials = async function findUserByCredentials(email, password) {
+  return this.findOne({ email })
+    .select('+password')
+    .then((user) => {
+      if (!user) {
+        throw new UnautorizedError('Неправильные почта или пароль');
+      }
+      return bcrypt.compare(password, user.password)
+        .then((matched) => {
+          if (!matched) {
+            throw new UnautorizedError('Неправильные почта или пароль');
+          }
+          return user;
+        });
+    });
+};
+
+module.exports = mongoose.model('user', userSchema);
